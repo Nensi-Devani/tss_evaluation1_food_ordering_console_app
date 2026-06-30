2f723672c1b166c9ff70cdb246835bb48591a47e
Nensi Devani <ndevani894@rku.ac.in>
2026-06-30 18:49:17 +0530

    define id prefix as constant

---
+++ src/main/java/com/foodorder/constants/IdConstants.java
@@ -0,0 +1,15 @@
+package com.foodorder.constants;
+
+public final class IdConstants {
+    public static final String USER_ID_PREFIX = "USR";
+    public static final String RESTAURANT_ID_PREFIX = "RES";
+    public static final String MENU_ITEM_ID_PREFIX = "MI";
+    public static final String CART_ID_PREFIX = "CRT";
+    public static final String CART_ITEM_ID_PREFIX = "CRTI";
+    public static final String ORDER_ID_PREFIX = "ORD";
+    public static final String ORDER_ITEM_ID_PREFIX = "ORDI";
+    public static final String PAYMENT_ID_PREFIX = "PAY";
+    public static final String ORDER_OTP_ID_PREFIX = "OTP";
+    public static final String DISCOUNT_ID_PREFIX = "DIS";
+    public static final String ORDER_LOG_ID_PREFIX = "LOG";
+}

bac0236598b2f4a8d10cbbcb4da159de2777a029
Nensi Devani <ndevani894@rku.ac.in>
2026-06-30 23:46:13 +0530

    add automatic id increment logic

--- src/main/java/com/foodorder/util/IdGenerator.java
+++ src/main/java/com/foodorder/util/IdGenerator.java
@@ -1,4 +1,21 @@
 package com.foodorder.util;
 
-public class IdGenerator {
+import com.foodorder.common.Identifiable;
+
+import java.util.List;
+
+public final class IdGenerator {
+    public static <T extends Identifiable> String generateId(String filePath, String prefix) {
+        List<T> data = FileUtil.readData(filePath);
+
+        if (data.isEmpty()) {
+            return prefix + "001";
+        }
+
+        String lastId = data.get(data.size() - 1).getId(); // get the last object id
+        String numericPart = lastId.replaceAll("\\D", ""); // removes everything except digits
+        int nextNumber = Integer.parseInt(numericPart) + 1; // increment by 1
+
+        return prefix + String.format("%03d", nextNumber);
+    }
 }

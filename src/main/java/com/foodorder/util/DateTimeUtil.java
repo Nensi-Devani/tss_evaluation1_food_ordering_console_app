2e250a6d6a067f4acbb75408aad8513e2a592405
Nensi Devani <ndevani894@rku.ac.in>
2026-07-01 00:36:20 +0530

    add local date-time with format

--- src/main/java/com/foodorder/util/DateTimeUtil.java
+++ src/main/java/com/foodorder/util/DateTimeUtil.java
@@ -1,4 +1,20 @@
 package com.foodorder.util;
 
-public class DateTimeUtil {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public final class DateTimeUtil {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+    public static LocalDateTime now() {
+        return LocalDateTime.now();
+    }
+
+    public static String format(LocalDateTime dateTime) {
+        if (dateTime == null) {
+            return "";
+        }
+
+        return dateTime.format(FORMATTER);
+    }
 }

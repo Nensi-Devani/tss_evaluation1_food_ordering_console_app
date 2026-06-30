27b998671fa62df91ccabb0ecf38a12860af5b5d
Nensi Devani <ndevani894@rku.ac.in>
2026-06-30 23:49:46 +0530

    add random otp generater logic

--- src/main/java/com/foodorder/util/OTPGenerator.java
+++ src/main/java/com/foodorder/util/OTPGenerator.java
@@ -1,4 +1,19 @@
 package com.foodorder.util;
 
-public class OTPGenerator {
+import com.foodorder.constants.AppConstants;
+
+import java.util.Random;
+
+public final class OTPGenerator {
+    private static final Random RANDOM = new Random();
+
+    public static String generateOTP() {
+        StringBuilder otp = new StringBuilder();
+
+        for (int i = 0; i < AppConstants.OTP_LENGTH; i++) {
+            otp.append(RANDOM.nextInt(10));
+        }
+
+        return otp.toString();
+    }
 }

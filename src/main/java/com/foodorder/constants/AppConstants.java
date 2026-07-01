b7092ab66a78349b6f03627b06acf97d983f8667
Nensi Devani <ndevani894@rku.ac.in>
2026-07-01 18:12:42 +0530

    rename variable

--- src/main/java/com/foodorder/constants/AppConstants.java
+++ src/main/java/com/foodorder/constants/AppConstants.java
@@ -6,7 +6,7 @@ public class AppConstants {
     public static final double DISCOUNT_ELIGIBLE_AMOUNT = 500.0;
     public static final double DISCOUNT_PERCENTAGE = 10.0;
 
-    public static final int MAX_ORDERS_PER_DELIVERY_BOY = 3;
+    public static final int MAX_ASSIGNED_ORDERS_PER_DELIVERY_BOY = 3;
 
     public static final double DELIVERY_CHARGE_UPTO_2_ITEMS = 20;
     public static final double DELIVERY_CHARGE_UPTO_4_ITEMS = 30;

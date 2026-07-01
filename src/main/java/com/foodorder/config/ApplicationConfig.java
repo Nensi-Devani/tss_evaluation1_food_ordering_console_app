2317ba7c94b240b0438f675e28bd85dcda43acd0
Nensi Devani <ndevani894@rku.ac.in>
2026-07-01 18:12:24 +0530

    remove space

--- src/main/java/com/foodorder/config/ApplicationConfig.java
+++ src/main/java/com/foodorder/config/ApplicationConfig.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.IOException;
 
 import com.foodorder.constants.FileConstants;
+import com.foodorder.enums.Role;
 import com.foodorder.facade.CheckoutFacade;
+import com.foodorder.model.User;
+import com.foodorder.repository.UserRepository;
 
 public class ApplicationConfig {
     public static CheckoutFacade checkoutFacade;
@@ -24,6 +27,8 @@ public class ApplicationConfig {
         createFile(FileConstants.PAYMENTS_FILE);
         createFile(FileConstants.DISCOUNTS_FILE);
 
+        initializeAdmin();
+
         System.out.println("System Initialized...");
     }
 
@@ -31,25 +36,31 @@ public class ApplicationConfig {
         try {
             File file = new File(path);
 
-            if (!file.exists()) {
+            if (!file.exists())
                 file.createNewFile();
-            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-}
 
-//package com.foodorder.config;
-//
-//import com.foodorder.facade.CheckoutFacade;
-//
-//public class ApplicationConfig {
-//    public static CheckoutFacade checkoutFacade;
-//
-//    public static void init() {
-//        checkoutFacade = new CheckoutFacade();
-//        System.out.println("System Initialized...");
-//    }
-//}
+    private static void initializeAdmin() {
+        UserRepository userRepository = new UserRepository();
+
+        User admin = userRepository.findByEmail("admin@gmail.com");
+
+        if (admin == null) {
+            admin = new User();
+
+            admin.setId("USR001");
+            admin.setName("Nensi-Admin");
+            admin.setEmail("admin@gmail.com");
+            admin.setPassword("123456");
+            admin.setRole(Role.ADMIN);
+
+            userRepository.save(admin);
+
+            System.out.println("Default Admin Created.");
+        }
+    }
+}

a47a5f1c0a3d13657172959f6ea03769c83d8740
Nensi Devani <ndevani894@rku.ac.in>
2026-07-01 09:20:31 +0530

    create app config

--- src/main/java/com/foodorder/config/ApplicationConfig.java
+++ src/main/java/com/foodorder/config/ApplicationConfig.java
@@ -1,4 +1,13 @@
 package com.foodorder.config;
 
+import com.foodorder.facade.CheckoutFacade;
+
 public class ApplicationConfig {
+    public static CheckoutFacade checkoutFacade;
+
+    public static void init() {
+        checkoutFacade = new CheckoutFacade();
+
+        System.out.println("System Initialized...");
+    }
 }

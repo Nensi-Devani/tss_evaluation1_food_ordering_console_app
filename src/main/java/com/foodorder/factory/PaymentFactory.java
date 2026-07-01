58b955fb58739c0fb619297eebeb3e420f21905e
Nensi Devani <ndevani894@rku.ac.in>
2026-07-01 10:42:31 +0530

    add payment factory logic

--- src/main/java/com/foodorder/factory/PaymentFactory.java
+++ src/main/java/com/foodorder/factory/PaymentFactory.java
@@ -1,4 +1,21 @@
 package com.foodorder.factory;
 
+import com.foodorder.enums.PaymentType;
+import com.foodorder.strategy.CashPaymentStrategy;
+import com.foodorder.strategy.PaymentStrategy;
+import com.foodorder.strategy.UpiPaymentStrategy;
+
 public class PaymentFactory {
+    public static PaymentStrategy getPaymentStrategy(PaymentType paymentType) {
+        switch (paymentType) {
+            case CASH:
+                return new CashPaymentStrategy();
+
+            case UPI:
+                return new UpiPaymentStrategy();
+
+            default:
+                throw new IllegalArgumentException("Invalid Payment Type");
+        }
+    }
 }

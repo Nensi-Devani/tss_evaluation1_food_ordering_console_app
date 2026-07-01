e6844e8a74c5ba54aa6e3163dde96adc1089c40e
Nensi Devani <ndevani894@rku.ac.in>
2026-07-01 12:25:07 +0530

    add startign point logic

--- src/main/java/com/foodorder/app/Main.java
+++ src/main/java/com/foodorder/app/Main.java
@@ -1,4 +1,16 @@
 package com.foodorder.app;
 
+import com.foodorder.config.ApplicationConfig;
+import com.foodorder.controller.LoginController;
+
 public class Main {
+    public static void main(String[] args) {
+        ApplicationConfig.init();
+
+        LoginController loginController = new LoginController();
+
+        while (true) {
+            loginController.start();
+        }
+    }
 }
--- src/main/java/com/foodorder/config/ApplicationConfig.java
+++ src/main/java/com/foodorder/config/ApplicationConfig.java
@@ -1,5 +1,9 @@
 package com.foodorder.config;
 
+import java.io.File;
+import java.io.IOException;
+
+import com.foodorder.constants.FileConstants;
 import com.foodorder.facade.CheckoutFacade;
 
 public class ApplicationConfig {
@@ -8,6 +12,44 @@ public class ApplicationConfig {
     public static void init() {
         checkoutFacade = new CheckoutFacade();
 
+        createFile(FileConstants.USERS_FILE);
+        createFile(FileConstants.RESTAURANTS_FILE);
+        createFile(FileConstants.MENU_ITEMS_FILE);
+        createFile(FileConstants.CARTS_FILE);
+        createFile(FileConstants.CART_ITEMS_FILE);
+        createFile(FileConstants.ORDERS_FILE);
+        createFile(FileConstants.ORDER_ITEMS_FILE);
+        createFile(FileConstants.ORDER_LOGS_FILE);
+        createFile(FileConstants.ORDER_OTPS_FILE);
+        createFile(FileConstants.PAYMENTS_FILE);
+        createFile(FileConstants.DISCOUNTS_FILE);
+
         System.out.println("System Initialized...");
     }
+
+    private static void createFile(String path) {
+        try {
+            File file = new File(path);
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+//package com.foodorder.config;
+//
+//import com.foodorder.facade.CheckoutFacade;
+//
+//public class ApplicationConfig {
+//    public static CheckoutFacade checkoutFacade;
+//
+//    public static void init() {
+//        checkoutFacade = new CheckoutFacade();
+//        System.out.println("System Initialized...");
+//    }
+//}

e15a30d6bb1eec899d4e1aba345f8360d8de69a0
Nensi Devani <ndevani894@rku.ac.in>
2026-07-20 11:28:04 +0530

    view order logs feature

--- src/main/java/com/foodorder/repository/OrderLogRepository.java
+++ src/main/java/com/foodorder/repository/OrderLogRepository.java
@@ -1,17 +1,31 @@
 package com.foodorder.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.foodorder.constants.FileConstants;
 import com.foodorder.constants.IdConstants;
 import com.foodorder.constants.MessageConstants;
+import com.foodorder.database.DatabaseConnection;
+import com.foodorder.enums.OrderLogAction;
 import com.foodorder.exception.OrderLogNotFoundException;
 import com.foodorder.model.OrderLog;
 import com.foodorder.util.FileUtil;
 import com.foodorder.util.IdGenerator;
 
 public class OrderLogRepository {
+    Connection connection;
+    PreparedStatement preparedStatement;
+
+    public OrderLogRepository(){
+        connection = DatabaseConnection
+                        .getInstance()
+                        .getConnection();
+    }
 
     public void save(OrderLog orderLog) {
         List<OrderLog> orderLogs = FileUtil.readData(FileConstants.ORDER_LOGS_FILE);
@@ -54,11 +68,28 @@ public class OrderLogRepository {
     public List<OrderLog> findByOrderId(String orderId) {
         List<OrderLog> filteredLogs = new ArrayList<>();
 
-        List<OrderLog> orderLogs = FileUtil.readData(FileConstants.ORDER_LOGS_FILE);
+        String query = "SELECT * FROM order_logs WHERE order_id = ?";
 
-        for (OrderLog orderLog : orderLogs) {
-            if (orderLog.getOrderId().equals(orderId))
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setLong(1, Long.parseLong(orderId));
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                OrderLog orderLog = new OrderLog();
+
+                orderLog.setId(String.valueOf(resultSet.getLong("order_log_id")));
+                orderLog.setOrderId(String.valueOf(resultSet.getLong("order_id")));
+                orderLog.setAction(OrderLogAction.valueOf(resultSet.getString("action")));
+                orderLog.setActionBy(String.valueOf(resultSet.getLong("action_taken_by")));
+                orderLog.setActionDateTime(
+                        resultSet.getTimestamp("action_date_time").toLocalDateTime()
+                );
+
                 filteredLogs.add(orderLog);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
         }
 
         return filteredLogs;
